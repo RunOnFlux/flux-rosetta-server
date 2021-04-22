36a012b0d5a686f922308964a366cd0759af23cc
SyGem Software <simon@sygem.com>
2021-04-22 11:52:44 +0100

    Remove DGB networks, add Flux Testnet

--- src/CustomNetworks.js
+++ src/CustomNetworks.js
@@ -16,51 +16,18 @@ const mainnet = {
   ],
 };
 
-const livenet = {
-  name: 'dgb_livenet',
-  alias: 'dgb_mainnet',
-  bech32prefix: 'dgb',
-  pubkeyhash: 0x1e,
-  privatekey: 0x80,
-  privatekeyOld: 0x9e,
-  scripthash: 0x3f,
-  scripthashTwo: 0x05,
-  xpubkey: 0x0488b21e,
-  xprivkey: 0x0488ade4,
-  networkMagic: 0xfac3b6da,
-  port: 12024,
-  dnsSeeds: [
-    'seed.digibyte.co',
-    'seed.digibyte.io',
-    'digiexplorer.info',
-  ],
-};
-
 const testnet = {
-  name: 'dgb_testnet',
-  alias: 'dgb_testnet',
-  bech32prefix: 'dgbt',
-  pubkeyhash: 0x7e,
-  privatekey: 0xfe,
-  scripthash: 0x8c,
+  name: 'flux_testnet',
+  pubkeyhash: 0x1d25,
+  privatekey: 0xef,
+  scripthash: 0x1cba,
   xpubkey: 0x043587cf,
   xprivkey: 0x04358394,
-};
-
-const regtest = {
-  name: 'dgb_regtest',
-  alias: 'dgb_regtest',
-  bech32prefix: 'dgbrt',
-  pubkeyhash: 0x7e,
-  privatekey: 0xfe,
-  scripthash: 0x8c,
-  xpubkey: 0x043587cf,
-  xprivkey: 0x04358394,
+  zaddr: 0x16b6,
+  zkey: 0xac08,
 };
 
 module.exports = {
-  livenet,
-  testnet,
-  regtest,
   mainnet,
+  testnet,
 };
